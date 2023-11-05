e7b5f506fe9f2e0099b98d50e2bb59714e751707
catheriny09 <catheriny09@gmail.com>
2023-11-04 22:44:38 -0700

    adding openai api calls

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import slack_sdk as slack
+import openai
 
 TOKEN = "xoxb-6159036217889-6146424057874-RZInAgnUziqX1087QhVnoNWJ"
+API_KEY = "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"
 
 def app():
     client = slack.WebClient(token=TOKEN)
+    openai.api_key = API_KEY
     
     NAME = input("Enter channel to summarize: ")
     MAX_MESSAGES = input("Max messages: ")
@@ -13,7 +16,17 @@ def app():
         messages, ok = getMessages(client, CHANNEL, MAX_MESSAGES)
         
         if ok:
-            print(messages)
+            transcript = createTranscript(messages)
+            p = "Please summarize this conversation: " + transcript
+            
+            response = openai.ChatCompletion.create(
+                engine="davinci",
+                prompt=p,
+                max_tokens=100
+            )
+            
+        summary = response.choices[0].text
+        print(summary)
         
         
 def validateChannel(client, name):
@@ -56,5 +69,14 @@ def getMessages(client, c, m):
     except Exception as e:
         print(f"Error: {e.response['error']}")
         return None, False
+    
+def createTranscript(messages):
+    transcript = ''
+    for message in messages: 
+        if message['type'] == 'message':
+            line = message['user'] + ': ' + message['text'] + '\n'
+            transcript += line
+            
+    return transcript
         
 app()
